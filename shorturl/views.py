915457babcbfe642b40806c23c15cd1185bebb4e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-07-18 18:13:19 -0500

    adsfadsf

--- shorturl/views.py
+++ shorturl/views.py
@@ -5,21 +5,26 @@ from shorturl.models import Url
 
 
 def index(request):
-	return render(request,'shorturl/index.html')
+	if request.method == 'POST':
+		pass
+	else:
+		return render(request,'shorturl/index.html')
 
 def convert_url(request):
-	print(request)
-	print(request)
-	print(request)
 	url = request.POST['url']
 	urlinstance = ShortUrl('wiliamfalfkafjafkafjk')
 	short_url = urlinstance.convert_url()
 
-	print("****************************************+")
-	print(short_url)
-	print(short_url)
-	print(short_url)
-	# pdb.set_trace()
+	if Url.objects.create(url=url,shortUrl=short_url):
+
+
+
+
+		print("****************************************+")
+		print(short_url)
+		print(short_url)
+		print(short_url)
+		# pdb.set_trace()
 
 
 	return render(request,'shorturl/index.html')
